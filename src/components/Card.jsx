48eb85aa20d2beed9e35ace152f717f116c9cc9d
Davi P. Oliveira <dpo.dev11@gmail.com>
2024-03-05 17:39:05 -0300

    added commit to the card jsx file and content js file

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,17 @@
 import GlobalStyles from "./styles/Global.styles"
-import content from "./content"
+import { StyledCard } from "./styles/Card.styled"
+import Qrcode from "../images/image-qr-code.png"
+
 
 const Card = () => {
   return (
-    <div>Card</div>
+    <StyledCard>
+      <div>
+        <img src={Qrcode} alt="qr code" />
+      </div>
+      <h3></h3>
+      <p></p>
+    </StyledCard>
   )
 }
 
---
+++ src/content.js
@@ -0,0 +1,4 @@
+const content = {
+  title: 'Improve your front-end skills by building projects',
+  text: 'Scan the QR code to visit Frontend Mentor and Take your coding skills to the next level'
+}
